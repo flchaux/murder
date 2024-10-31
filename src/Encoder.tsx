2b9ea59f9667110c5c27c823dde3e53d391d88de
Florian Chaux <chaux.florian@gmail.com>
2024-10-31 22:04:43 +0100

    encoder

--- src/Encoder.tsx
+++ src/Encoder.tsx
@@ -1,41 +1,48 @@
-import { Stack, TextField } from "@mui/material";
+import { Box, Stack, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 
-const shift = 20;
+const shift = { x: 5, y: 3 };
+const lineSize = shift.x * 2 + 1;
+
+function randomChar() {
+    return Math.random() > 0.2 ? String.fromCharCode(65 + Math.floor(Math.random() * 26)) : ' ';
+}
 
 export function Encoder() {
     const [value, setValue] = useState("");
-    const [encoded, setEncoded] = useState("");
-    const [decoded, setDecoded] = useState("");
+    const [encoded, setEncoded] = useState<string[][]>([[]]);
+    const maxLenght = ((shift.x + 1) * shift.y) * 2;
     useEffect(() => {
-        let e = "";
-        for (let i = 0; i < value.length; i++) {
-            if (i % 2 === 0) {
-                e += value[i].toUpperCase();
-            }
-            if (i % 2 === 1 && i >= shift) {
-                e += value[i - shift].toUpperCase();
+        let e = Array.from({ length: shift.y * 2 }, () => Array.from({ length: lineSize }, () => randomChar()));
+        let x = 0;
+        let y = 0;
+        for (let i = 0; i < value.slice(0, maxLenght).length; i += 2) {
+            e[y][x] = value[i];
+            if (i + 1 >= value.length) break;
+            e[y + shift.y][x + shift.x] = value[i + 1];
+            x = (x + 1) % (shift.x + 1);
+            if (x == 0) {
+                y++;
             }
         }
         setEncoded(e);
-        let d = "";
-        for (let i = 0; i < e.length; i++) {
-            if (i + shift < e.length) {
-                d += e[i];
-                d += e[i + shift];
-            }
-        }
-        setDecoded(d);
     }, [value]);
+
     return <Stack spacing={1} margin={2}>
         Text to encode
         <TextField multiline value={value} onChange={(v) => setValue(v.target.value)} />
-        Good encoding
-        <TextField multiline />
+        {value.length} / {maxLenght}<br />
         Text encoded
-        <div style={{ width: 800, border: "solid 1px black", height: 200 }}>{encoded}</div>
-        Text decoded
-        <div style={{ width: 800, border: "solid 1px black", height: 200 }}>{decoded}</div>
+        <Box>
+            <table>
+                {encoded.map((e, x) => (
+                    <tr key={x}>
+                        {e.map((c, y) => (
+                            <td key={y} style={{ width: 50, height: 50, textAlign: "center" }}>{c}</td>
+                        ))}
+                    </tr>)
+                )}
+            </table></Box>
     </Stack>
 }
 
